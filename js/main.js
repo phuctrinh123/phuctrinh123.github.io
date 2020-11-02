c06cedce8da69c99adf4db1a52b9bc7dacf6bfe2
phuctrinh123 <41340011+phuctrinh123@users.noreply.github.com>
2020-11-02 17:20:46 +0700

    Add files via upload
    upload static web

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let window_Width = window.innerWidth;
+let window_Height = window.innerHeight;
+
+document.body.style.width = window_Width + "px";
+document.body.style.height = window_Height + "px";
+
+document.body.style.fontSize = Math.round(window_Width*18/375);
+
+let logo = document.getElementById('logo-scene');
+let intro = document.getElementById('intro-scene');
+let direction = document.getElementById('direction-scene');
+let menu = document.getElementById('menu-scene');
+let cup = document.getElementById('cup');
+let question = document.getElementById('question');
+let slogan = document.getElementById('slogan');
+let letMeOrderButton = document.getElementById('letMeOrderButton');
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    setTimeout(()=>{
+        logo.classList.add("show");
+        setTimeout(()=>{
+            logo.classList.remove("show");
+            setTimeout(()=>{
+                cup.classList.add("show");
+                setTimeout(()=>{
+                    question.classList.add("show");
+                    setTimeout(()=>{
+                        slogan.classList.add("show");
+                        setTimeout(()=>{
+                            intro.classList.add("hide");
+                            direction.style.display = "flex";
+                            setTimeout(()=>{
+                                logo.style.display = "none";
+                                intro.style.display = "none";
+                                direction.classList.add("show");
+                            },500);
+                        },2000);
+                    },200);
+                },200);
+            },500);
+        },1500);
+    },200);
+});
+
+letMeOrderButton.addEventListener("click",()=>{
+    direction.classList.remove("show");
+    setTimeout(()=>{
+        menu.classList.add("show");
+    },1000);
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoWidth: true
+    });
+});
