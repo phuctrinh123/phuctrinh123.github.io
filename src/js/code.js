3f4be1c49ea69f15f8cddf8f81c4a24ea1d48e9a
phuctrinh123 <phuctrinh.work@gmail.com>
2021-04-14 00:01:38 +0700

    Finish the candy board

--- src/js/code.js
+++ src/js/code.js
@@ -1,4 +1,5 @@
 var viewportWidth = window.screen.width;
+var allLoaded =  false;
 
 function blockTap(index){
     var block = document.getElementById(`block-${index}`);
@@ -12,8 +13,8 @@ function boardInit (){
     var htmlString = ""
     var rowCount = 0;
 
-    board.style.width = viewportWidth -10;
-    board.style.height = viewportWidth -10;
+    board.style.width = viewportWidth - Math.floor(boardWidth*50/375);
+    board.style.height = viewportWidth - Math.floor(boardWidth*50/375);
 
     for (let i = 1; i <= total; i++) {
         htmlString += `
@@ -21,14 +22,14 @@ function boardInit (){
                 class="block normal" 
                 id="block-${i}" 
                 style="
-                    width:${Math.round(boardWidth/3) - 17}px;
-                    margin-top: ${rowCount > 0 ? "11px" : 0};
-                    margin-left: ${(i - (rowCount*3))%2 == 0 || i%3 == 0 ? "11px" : "0px"};
+                    width:${Math.floor(boardWidth/3) - Math.floor(boardWidth*25/375)}px;
+                    margin-top: ${rowCount > 0 ? Math.floor(boardWidth*10/375) + "px" : 0};
+                    margin-left: ${(i - (rowCount*3))%2 == 0 ? Math.floor(boardWidth*10/375) + "px" : i%3 == 0 ? Math.floor(boardWidth*10/375) +"px" : "0px"};
                 " 
                 onClick="blockTap(${i})"
             > \n
-                <image src="src/images/block-${i}.png" />
-                <image src="src/images/block-${i}-right.png"/>
+                <img src="src/images/block-${i}.png" />
+                <img src="src/images/block-${i}-right.png"/>
             </div> \n
         `;  
 
@@ -44,3 +45,24 @@ function boardInit (){
 }
 
 boardInit();
+
+window.addEventListener("load", event => {
+    var image = document.getElementsByTagName('img');
+    for(let i = 0; i < image.length; i++){
+        if(image[i].complete && image[i].naturalHeight !== 0){
+            allLoaded = true;
+        }
+        else{
+            allLoaded = false;
+        }   
+    } 
+
+    if(allLoaded){
+        var board = document.getElementById("board");
+        board.className += " show"
+        for(let i = 1; i <= image.length/2; i++){
+            var block = document.getElementById(`block-${i}`);
+            block.className += " show";
+        }
+    }
+});
