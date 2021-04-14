04e1701518acef841e9718be73540200f8ce54cd
phuctrinh123 <phuctrinh.work@gmail.com>
2021-04-14 15:24:48 +0700

    Kiếm tra vụ dynamic add element không run css bình thường được

--- src/js/code.js
+++ src/js/code.js
@@ -69,8 +69,7 @@ function boardInit (){
                     width:${Math.floor(boardWidth/3) - Math.floor(boardWidth*25/375)}px;
                     margin-top: ${rowCount > 0 ? Math.floor(boardWidth*10/375) + "px" : 0};
                     margin-left: ${(i - (rowCount*3))%2 == 0 ? Math.floor(boardWidth*10/375) + "px" : i%3 == 0 ? Math.floor(boardWidth*10/375) +"px" : "0px"};
-                " 
-                onClick="blockTap(${i})"
+                "
             > \n
                 <img src="src/images/block-${i}.png" />
                 <img src="src/images/block-${i}-right.png"/>
@@ -85,6 +84,13 @@ function boardInit (){
             htmlString = "";
             rowCount += 1;
         }        
+    }
+
+    for(let i = 1; i <= total; i++){
+        let block = document.getElementById(`block-${i}`);
+        block.addEventListener("click", ()=>{
+            blockTap(i);
+        });
     }
 }
 
